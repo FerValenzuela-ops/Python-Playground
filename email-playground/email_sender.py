4e70f43e3597d5d72bc0586be1fa3bb45531be5b
Fernando Valenzuela <73801683+FerValenzuela-ops@users.noreply.github.com>
2021-06-07 21:18:04 -0400

    Update email_sender.py

--- email-playground/email_sender.py
+++ email-playground/email_sender.py
@@ -9,9 +9,9 @@ html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 
-email['from'] = 'Fernando'
-email['to'] = 'games@startgaming.net'
-email['subject'] = '[AGAIN] Netflix problems'
+email['from'] = 'Your name'
+email['to'] = 'Email of that person/entity'
+email['subject'] = 'The subject '
 
 
 email.set_content(html.substitute({'name':'TinTin'}), 'html')
@@ -19,7 +19,7 @@ email.set_content(html.substitute({'name':'TinTin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    # smtp.login('pyferdevs@gmail.com' , 'justforfun69420')
-    smtp.login('fernandox240997@gmail.com' , 'Superclave2mil.')
+    # smtp.login('email@gmail.com' , 'password')
+    smtp.login('email@gmail.com' , 'password')
     smtp.send_message(email)
     print('All good boss')
